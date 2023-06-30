964f840791ac01c98d2970dad498e293ffd34259
Parkjaeseong94 <wotjddml159@gmail.com>
2023-06-30 15:06:37 +0900

    first commit

---
+++ server/src/main/java/re21/ieun/ieun/IeunApplication.java
@@ -0,0 +1,13 @@
+package re21.ieun.ieun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IeunApplication.class, args);
+	}
+
+}
