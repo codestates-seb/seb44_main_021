9853b5c033a341765350df4f4cce8372716d5105
Parkjaeseong94 <wotjddml159@gmail.com>
2023-07-01 00:46:45 +0900

    git actions 자동화 배포 마지막 확인

--- src/main/java/re21/ieun/ieun/IeunApplication.java
+++ src/main/java/re21/ieun/ieun/IeunApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class IeunApplication {
 
