f9616eafed4d53702a4beb38d00e3c0e98ae35d4
Parkjaeseong94 <wotjddml159@gmail.com>
2023-07-01 00:30:22 +0900

    git actions 자동화 배포 성공

--- src/main/java/re21/ieun/ieun/IeunApplication.java
+++ src/main/java/re21/ieun/ieun/IeunApplication.java
@@ -2,9 +2,19 @@ package re21.ieun.ieun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
+@Controller
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class IeunApplication {
+
+
+	@RequestMapping("/")
+	String home(){
+		return "hello world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IeunApplication.class, args);
