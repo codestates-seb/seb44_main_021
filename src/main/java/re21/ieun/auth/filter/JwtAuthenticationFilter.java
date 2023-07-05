37da6e2af55790ee2c1d1074bb88fd2b704f9f3e
yunkr <seunghyun@gmail.com>
2023-07-05 16:13:40 +0900

    Cookie 추가 구현

--- src/main/java/re21/ieun/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/re21/ieun/auth/filter/JwtAuthenticationFilter.java
@@ -57,6 +57,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         response.setHeader("Authorization", "Bearer " + accessToken);
         response.setHeader("Refresh", refreshToken);
         response.setHeader("MemberId", memberId);
+        //response.setHeader("DisplayName", DisplayName);
 
         this.getSuccessHandler().onAuthenticationSuccess(request, response, authResult);
     }
