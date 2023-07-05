599f4e49f5b58381f665fbc4736567f069e024b6
yunkr <seunghyun@gmail.com>
2023-07-05 15:13:18 +0900

    Cookie 추가 구현

--- src/main/java/re21/ieun/config/SecurityConfiguration.java
+++ src/main/java/re21/ieun/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
 import re21.ieun.auth.filter.JwtAuthenticationFilter;
 import re21.ieun.auth.filter.JwtVerificationFilter;
 import re21.ieun.auth.handler.MemberAccessDeniedHandler;
@@ -24,6 +25,12 @@ import re21.ieun.auth.jwt.JwtTokenizer;
 import re21.ieun.auth.utils.CustomAuthorityUtils;
 import re21.ieun.member.repository.MemberRepository;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -108,8 +115,30 @@ public class SecurityConfiguration {
             JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
             builder
                     .addFilter(jwtAuthenticationFilter)
-                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class)
+                    .addFilterAfter(new CustomResponseCookieFilter(), JwtVerificationFilter.class); // CustomResponseCookieFilter 추가
 
+
+        }
+    }
+
+    // cookie 추가
+    public static class CustomResponseCookieFilter extends OncePerRequestFilter {
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            // 여기에서 필요한 로직을 구현하여 쿠키를 생성하고 설정합니다.
+            String cookieName = "your_cookie_name";
+            String cookieValue = "your_cookie_value";
+            Cookie cookie = new Cookie(cookieName, cookieValue);
+            cookie.setPath("/"); // 쿠키의 경로 설정
+            cookie.setHttpOnly(true); // HTTP Only 설정 (JavaScript로 쿠키에 접근 불가)
+            cookie.setMaxAge(3600); // 쿠키의 유효 시간 설정 (초 단위, 여기서는 1시간)
+
+            // 응답 헤더에 쿠키 추가
+            response.addCookie(cookie);
+
+            // 다음 필터 또는 요청 처리로 이동
+            filterChain.doFilter(request, response);
         }
     }
 
