db51f7f888a669ccf6b314cc92cd0d3ce4cc39c0
yejinzz <yejin1320@naver.com>
2023-07-07 11:25:35 +0900

    [feat] close 버튼 추가

--- client/src/components/Modal/EditModal.js
+++ client/src/components/Modal/EditModal.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Style from "./EditModal.module.css";
+import CloseIcon from "@mui/icons-material/Close";
 
 const EditModal = ({ onClose }) => {
   return (
     <div id={Style.modalContainer}>
       <div className={Style.modalWrapper}>
+        <CloseIcon className={Style.closeIcon} onClick={onClose} />
         <div className={Style.imgUpload}>
           <img
             className={Style.userImg}
@@ -18,7 +20,7 @@ const EditModal = ({ onClose }) => {
           />
         </div>
         <div className={Style.modalContent}>
-          <p className={Style.edit}>닉네임</p>
+          <label>닉네임</label>
           <input type="text" />
           <label>이메일</label>
           <p>yejin123@gmail.com</p>
@@ -29,9 +31,7 @@ const EditModal = ({ onClose }) => {
           <label>새 비밀번호 확인</label>
           <input type="password" />
         </div>
-        <button className={Style.editButton} onClick={onClose}>
-          Edit
-        </button>
+        <button className={Style.editButton}>Edit</button>
       </div>
       <div className={Style.modalLayer} onClick={onClose}></div>
     </div>
