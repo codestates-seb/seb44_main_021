606b0ac9c64ac200289905f869c78b43a6f5f73f
weed7968 <tlstjdcjf980809@gmail.com>
2023-07-10 15:53:44 +0900

    [init] 초기상태로 되돌리기

--- client/src/page/Funding/FundingDetail.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FundingDetail = () => {
-  return <div>FundingDetail</div>;
-};
-
-export default FundingDetail;
--- client/src/page/Funding/FundingPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FundingPage = () => {
-  return <div>FundingPage</div>;
-};
-
-export default FundingPage;
