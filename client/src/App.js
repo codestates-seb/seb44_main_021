823200dd6ae169fcd051d5fe7d98e9aad8d59797
weed7968 <97829172+weed7968@users.noreply.github.com>
2023-07-10 15:15:12 +0900

    Revert "[Feat]메인 페이지"

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import MainPage from "./page/Main/MainPage";
-import UserSelectionPage from "./page/SignupPage/UserSelectionPage";
-import SignupPage from "./page/SignupPage/SignupPage";
-import StorePage from "./page/Store/StorePage";
-import FundingPage from "./page/Funding/FundingPage";
-import AboutPage from "./page/About/AboutPage";
-
 function App() {
-  return (
-    <div>
-      <Routes>
-        {/* <Route element={< />} path="/" /> */}
-        <Route element={<MainPage />} path="/" />
-        <Route element={<StorePage />} path="/store" />
-        <Route element={<FundingPage />} path="/funding" />
-        <Route element={<AboutPage />} path="/about" />
-        <Route element={<UserSelectionPage />} path="/signup" />
-        <Route element={<SignupPage />} path="/signup/*" />
-      </Routes>
-    </div>
-  );
+  return <div className="App">초기세팅</div>;
 }
 
 export default App;
