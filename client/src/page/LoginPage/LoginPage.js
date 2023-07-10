700b0fbc203ac9472d253c8e22b24a87c55f8d99
yejinzz <yejin1320@naver.com>
2023-07-10 15:22:50 +0900

    [feat] 로그인 성공시 엑세스토큰 로컬스토리지 저장 코드 추가

--- client/src/page/LoginPage/LoginPage.js
+++ client/src/page/LoginPage/LoginPage.js
@@ -1,27 +1,32 @@
 import { useState } from "react";
 import Style from "./LoginPage.module.css";
 import axios from "axios";
-import { useCookies } from "react-cookie";
+// import { useCookies } from "react-cookie";
+import Logo from "../../components/Logo/Logo";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [loginInfo, setLoginInfo] = useState({
     username: "",
     password: "",
   });
-  const [cookies, setCookie] = useCookies();
+  // const [cookies, setCookie] = useCookies();
+  const navigate = useNavigate();
 
   const handleInputValue = (key) => (e) => {
     setLoginInfo({ ...loginInfo, [key]: e.target.value });
-    console.log(loginInfo);
+    // console.log(loginInfo);
+  };
+
+  const handleButtonClick = () => {
+    // 페이지 이동 로직을 여기에 작성
+    navigate("/signup"); // 원하는 경로로 이동
   };
 
   const AxiosLogin = (e) => {
     e.preventDefault();
     axios
-      .post(
-        "http://ec2-43-201-105-214.ap-northeast-2.compute.amazonaws.com:8080/auth/login",
-        loginInfo
-      )
+      .post("/auth/login", loginInfo)
 
       .then((res) => {
         console.log(res);
@@ -30,16 +35,17 @@ const LoginPage = () => {
           // 엑세스 토큰 저장
           const authHeader = res.headers.authorization;
           const accessToken = authHeader.split(" ")[1];
+          localStorage.setItem("token", accessToken);
           // 쿠키에 리프레시 토큰 저장
-          const refreshToken = res.headers.refresh;
-          setCookie("refreshToken", refreshToken, { path: "/" });
+          // const refreshToken = res.headers.refresh;
+          // setCookie("refreshToken", refreshToken, { path: "/" });
 
-          // axios.defaults.headers.common[
-          //   "Authorization"
-          // ] = `Bearer ${accessToken}`;
+          axios.defaults.headers.common[
+            "Authorization"
+          ] = `Bearer ${localStorage.getItem("token")}`;
 
           console.log(accessToken);
-          console.log(cookies.refreshToken);
+          // console.log(cookies.refreshToken);
         }
       })
       .catch((err) => {
@@ -50,22 +56,30 @@ const LoginPage = () => {
 
   return (
     <div id={Style.loginContainer} onSubmit={AxiosLogin}>
-      <form id={Style.loginInputContainer}>
-        <input
-          className={Style.loginInput}
-          type="email"
-          placeholder="이메일을 입력해주세요."
-          onChange={handleInputValue("username")}
-        ></input>
-
-        <input
-          className={Style.loginInput}
-          type="password"
-          placeholder="비밀번호를 입력해주세요."
-          onChange={handleInputValue("password")}
-        ></input>
+      <form className={Style.loginWrapper}>
+        <Logo />
+        <label>
+          이메일
+          <input
+            type="email"
+            placeholder="이메일을 입력해주세요."
+            onChange={handleInputValue("username")}
+          ></input>
+        </label>
+        <label>
+          비밀번호
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            onChange={handleInputValue("password")}
+          ></input>
+        </label>
         <button type="submit" className={Style.loginButton}>
-          LOGIN
+          login
+        </button>
+        <p>아직 이은의 회원이 아니라면,</p>
+        <button className={Style.signupButton} onClick={handleButtonClick}>
+          signup
         </button>
       </form>
     </div>
