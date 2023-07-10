a5ca170f5656dbc5bf501710a56fe2a8cad18a4a
weed7968 <tlstjdcjf980809@gmail.com>
2023-07-10 15:11:41 +0900

    [feat] 라우터 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-// import MainPage from "./page/Main/MainPage";
+import MainPage from "./page/Main/MainPage";
 import UserSelectionPage from "./page/SignupPage/UserSelectionPage";
 import SignupPage from "./page/SignupPage/SignupPage";
+import StorePage from "./page/Store/StorePage";
+import FundingPage from "./page/Funding/FundingPage";
+import AboutPage from "./page/About/AboutPage";
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route element={<MainPage />} path="/" /> */}
         {/* <Route element={< />} path="/" /> */}
+        <Route element={<MainPage />} path="/" />
+        <Route element={<StorePage />} path="/store" />
+        <Route element={<FundingPage />} path="/funding" />
+        <Route element={<AboutPage />} path="/about" />
         <Route element={<UserSelectionPage />} path="/signup" />
         <Route element={<SignupPage />} path="/signup/*" />
       </Routes>
