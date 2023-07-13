aecd5f70ed642a7dd0a4c7ac03193b2ffdc3d0b3
yejinzz <yejin1320@naver.com>
2023-07-13 17:14:45 +0900

    [feat]비밀번호 유효성 검사 및 프로필 수정

--- client/src/components/Modal/EditModal.jsx
+++ client/src/components/Modal/EditModal.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import { useState } from "react";
 import Style from "./EditModal.module.css";
 import CloseIcon from "@mui/icons-material/Close";
+import axios from "axios";
 
-const EditModal = ({ onClose }) => {
+const EditModal = ({ onClose, userData }) => {
+  const [editUserInfo, setEditUserInfo] = useState({
+    username: userData.displayName,
+    password: "",
+    verifyPwd: "",
+  });
+  const [newPwdErrMsg, setNewPwdErrMsg] = useState("");
+
+  const EditInputValue = (key) => (e) => {
+    setEditUserInfo({ ...editUserInfo, [key]: e.target.value });
+    console.log(editUserInfo);
+  };
+
+  const IsValidPwd = () => {
+    if (editUserInfo.password !== editUserInfo.verifyPwd) {
+      setNewPwdErrMsg("새 비밀번호와 일치하지 않습니다.");
+    } else {
+      setNewPwdErrMsg("");
+    }
+  };
+
+  // console.log(editUserInfo);
+  const AxiosPatch = (e) => {
+    e.preventDefault();
+    const { username, password } = editUserInfo;
+
+    if (newPwdErrMsg === "") {
+      axios
+        .patch(`/members/${userData.memberId}`, { username, password })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      onClose();
+    }
+  };
+
   return (
     <div id={Style.modalContainer}>
       <div className={Style.modalWrapper}>
@@ -21,17 +60,28 @@ const EditModal = ({ onClose }) => {
         </div>
         <div className={Style.modalContent}>
           <label>닉네임</label>
-          <input type="text" />
+          <input
+            type="text"
+            defaultValue={userData.displayName}
+            onChange={EditInputValue("username")}
+          />
           <label>이메일</label>
-          <p>yejin123@gmail.com</p>
+          <p>{userData.email}</p>
           <label>현재 비밀번호</label>
           <input type="password" />
           <label>새 비밀번호</label>
-          <input type="password" />
+          <input type="password" onChange={EditInputValue("password")} />
           <label>새 비밀번호 확인</label>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={EditInputValue("verifyPwd")}
+            onBlur={IsValidPwd}
+          />
+          {newPwdErrMsg && <p className={Style.errMsg}>{newPwdErrMsg}</p>}
         </div>
-        <button className={Style.editButton}>Edit</button>
+        <button className={Style.editButton} onClick={AxiosPatch}>
+          Edit
+        </button>
       </div>
       <div className={Style.modalLayer} onClick={onClose}></div>
     </div>
