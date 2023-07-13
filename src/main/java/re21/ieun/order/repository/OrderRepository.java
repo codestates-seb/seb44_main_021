16961499dec2c1dfd4df70d0a5940b96f690c01a
yunkr <seunghyun@gmail.com>
2023-07-13 10:49:42 +0900

    Order Repository 수정

--- src/main/java/re21/ieun/order/repository/OrderRepository.java
+++ src/main/java/re21/ieun/order/repository/OrderRepository.java
@@ -1,13 +1,15 @@
 package re21.ieun.order.repository;
 
-import gohome.dailydaily.domain.order.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import re21.ieun.order.entity.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @EntityGraph(attributePaths = {"orderProducts", "orderProducts.product", "orderProducts.option", "orderProducts.product.seller.member"})
-    Page<Order> findOrderByMember_Id(Long memberId, Pageable pageable);
+    //@EntityGraph(attributePaths = {"orderSells", "orderSells.sell", "orderProducts.option", "orderProducts.product.seller.member"})
+    Page<Order> findOrderByMemberId(Long memberId, Pageable pageable);
 }
