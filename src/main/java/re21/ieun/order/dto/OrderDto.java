761dda1bbb9a96bdf7f681452323b7da0d9ee347
강병주 <bjookang@naver.com>
2023-07-13 09:56:50 +0900

    FEATURE : 오더 기능 구현

---
+++ src/main/java/re21/ieun/order/dto/OrderDto.java
@@ -0,0 +1,29 @@
+package re21.ieun.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderDto {
+
+    @Getter
+    public static class Post {
+        @NotEmpty
+        private List<OrderProductDto.Post> orderProducts;
+
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response implements Serializable {
+        private Long orderId;
+        private Long orderNumber;
+        private String status;
+        private List<OrderProductDto.Response> orderProducts;
+        private LocalDateTime createdAt;
+    }
+}
