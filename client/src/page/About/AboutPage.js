de4267e62031dc384ba15753ca5f2782906629e3
yejinzz <yejin1320@naver.com>
2023-07-24 15:01:12 +0900

    [design]about page 완

--- client/src/page/About/AboutPage.js
+++ client/src/page/About/AboutPage.js
@@ -25,29 +25,130 @@ const AboutPage = () => {
         </div>
 
         <div className={styles.teamInfoContainer}>
-          <h1>Our team</h1>
+          <div className={styles.title}>
+            <h1>Our team</h1>
+            <h1>Re:21</h1>
+          </div>
+
           <div className={styles.teamInfowrapper}>
             <div className={styles.teamInfo}>
-              <img src={`${process.env.PUBLIC_URL}/image/성철.png`} />
+              <img
+                src={`${process.env.PUBLIC_URL}/image/성철.png`}
+                alt="team-img"
+              />
+              <p>FE 신성철</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px" }}>Work Log</p>
+                <ul>
+                  <li>Main Page</li>
+                  <li>Funding Detail Page</li>
+                  <li>Store Detail Page</li>
+                  <li>Funding List Page</li>
+                  <li>Store List Page</li>
+                </ul>
+              </div>
             </div>
             <div className={styles.teamInfo}>
-              <img src={`${process.env.PUBLIC_URL}/image/시현.jpg`} />
+              <img
+                src={`${process.env.PUBLIC_URL}/image/시현.jpg`}
+                alt="team-img"
+              />
+              <p>FE 곽시현</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px" }}>Work Log</p>
+                <ul>
+                  <li>Funding Create Page</li>
+                  <li>Store Create Page</li>
+                  <li>Funding Edit Page</li>
+                  <li>Store Edit Page</li>
+                </ul>
+              </div>
             </div>
             <div className={styles.teamInfo}>
-              <img src={`${process.env.PUBLIC_URL}/image/예진.jpg`} />
+              <img
+                src={`${process.env.PUBLIC_URL}/image/예진.jpg`}
+                alt="team-img"
+              />
+              <p>FE 나예진</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px" }}>Work Log</p>
+                <ul>
+                  <li>SignUp page</li>
+                  <li>LogIn Page</li>
+                  <li>My Page</li>
+                </ul>
+              </div>
             </div>
 
-            <div className={styles.teamInfo}></div>
-            <div className={styles.teamInfo}></div>
-            <div className={styles.teamInfo}></div>
+            <div className={styles.teamInfo}>
+              <img
+                src={`${process.env.PUBLIC_URL}/image/병주.jpg`}
+                alt="team-img"
+              />
+              <p>BE 강병주</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px" }}>Work Log</p>
+                <ul>
+                  <li>상품(Sell)</li>
+                  <li>order(주문)</li>
+                  <li>OAuth2(구글)</li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.teamInfo}>
+              <img
+                src={`${process.env.PUBLIC_URL}/image/재성.jpg`}
+                alt="team-img"
+              />
+              <p>BE 박재성</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px" }}>Work Log</p>
+                <ul>
+                  <li>배포</li>
+                  <li>
+                    깃허브 액션
+                    <br />
+                    (자동 배포)
+                  </li>
+                  <li>이미지</li>
+                  <li>회원가입(메일)</li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.teamInfo}>
+              <img
+                src={`${process.env.PUBLIC_URL}/image/승현.jpg`}
+                alt="team-img"
+              />
+              <p>BE 윤승현</p>
+              <div class={styles.arrowBox}>
+                <p style={{ marginBottom: "10px", fontSize: "16px" }}>
+                  Work Log
+                </p>
+                <ul>
+                  <li>업사이클링</li>
+                  <li>펀딩</li>
+                  <li>검색기능</li>
+                  <li>
+                    로그인
+                    <br />
+                    (access token)
+                    <br />
+                    local storage
+                  </li>
+                  <li>로그아웃(Redis)</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className={styles.companyInfo}>
+        {/* <div className={styles.companyInfo}>
           <h1>Company Info</h1>
           <div className={styles.infoBox}>dddd</div>
-        </div>
+        </div> */}
       </div>
+      <div id={styles.footer}>IEUN CO.</div>
     </div>
   );
 };
