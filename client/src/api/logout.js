cc041fee89dfad42dde66414eb6a62d36d730df4
yejinzz <yejin1320@naver.com>
2023-09-01 15:09:29 +0900

    [fix] 로그아웃시 리다이렉트 오류 수정

--- client/src/api/logout.js
+++ client/src/api/logout.js
@@ -9,14 +9,16 @@ export const logout = () => {
     })
     .then((res) => {
       localStorage.removeItem("login");
-      window.location.reload("/");
+      localStorage.removeItem("token");
+      window.location.replace("/");
       console.log(res);
     })
     .catch((err) => {
       if (err.response && err.response.status === 500) {
         // 토큰 만료로 인한 500 Unauthorized 에러일 경우에도 로그아웃 실행
         localStorage.removeItem("login");
-        window.location.reload("/");
+        localStorage.removeItem("token");
+        window.location.replace("/");
       } else {
         console.log("Error occurred during logout:", err);
       }
