6e82237de1ebd285072d634929255f8faf5836b7
Parkjaeseong94 <wotjddml159@gmail.com>
2024-01-16 18:56:16 +0900

    서버 재가동

--- server/src/main/java/re21/ieun/ieunApplication.java
+++ server/src/main/java/re21/ieun/ieunApplication.java
@@ -3,7 +3,7 @@ package re21.ieun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+//
 @EnableJpaAuditing
 @SpringBootApplication
 public class ieunApplication {
